21923bc51c78f1966082e01c1a423007c9323661
kaischaefer2000 <kai305@web.de>
2022-08-21 17:39:48 +0200

    Better comments in middleware file

--- middleware.js
+++ middleware.js
@@ -1,22 +1,23 @@
-import { getToken } from "next-auth/jwt";
-import { NextResponse } from "next/server";
+import { getToken } from 'next-auth/jwt';
+import { NextResponse } from 'next/server';
 
 const secret = process.env.JWT_SECRET;
 
-export async function middleware(req ) {
-  // token will exist if user is logged in
+export async function middleware(req) {
+  // The jwt token will exist, if user is logged in
   const token = await getToken({ req, secret });
 
-  // the path of the request
+  // The path of the request
   const { pathname } = req.nextUrl;
 
-  // Allow the request if its a request for next-auth session & provider fetching and the token exists
+  // Allow the request if its a request for next-auth session & provider fetching
+  // and also ifthe token exists
   if (pathname.includes('/api/auth') || pathname.includes('/_next') || token) {
     return NextResponse.next();
   }
 
   // redirect to login page, when disallowed request
   if (!token && pathname !== '/login') {
-   return NextResponse.redirect(new URL('/login', req.url));
+    return NextResponse.redirect(new URL('/login', req.url));
   }
 }
